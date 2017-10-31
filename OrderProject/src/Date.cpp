f56b67279a0f23b76278635a800ac1b4a0ec66f9
Trevor Ahlberg <trevorjahlberg@gmail.com>
2017-10-31 14:41:30 -0700

    Added date class

---
+++ OrderProject/src/Date.cpp
@@ -0,0 +1,18 @@
+/*
+ * Date.cpp
+ *
+ *  Created on: Oct 26, 2017
+ *      Author: CounterbalanceLT
+ */
+
+#include "Date.hpp"
+
+Date::Date() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Date::~Date() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ OrderProject/src/Date.hpp
@@ -0,0 +1,17 @@
+/*
+ * Date.hpp
+ *
+ *  Created on: Oct 26, 2017
+ *      Author: CounterbalanceLT
+ */
+
+#ifndef DATE_HPP_
+#define DATE_HPP_
+
+class Date {
+public:
+	Date();
+	virtual ~Date();
+};
+
+#endif /* DATE_HPP_ */
