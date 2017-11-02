31d4404d84085649749432e6b091764e468e97db
Trevor Ahlberg <trevorjahlberg@gmail.com>
2017-11-02 13:19:46 -0700

    member accessors for mediaItem

--- OrderProject/src/MediaItem.cpp
+++ OrderProject/src/MediaItem.cpp
@@ -16,3 +16,21 @@ MediaItem::~MediaItem() {
 	// TODO Auto-generated destructor stub
 }
 
+void MediaItem::setAuthorName(string str){
+	this->authorName = str;
+}
+void MediaItem::setPublicationDate(Date aDate){
+	this->publicationDate = aDate;
+}
+void MediaItem::setISBNNumber(string str){
+	this->ISBNNumber = str;
+}
+string MediaItem::getAuthorName(void){
+	return this->authorName;
+}
+Date MediaItem::getPublicationDate(void){
+	return this->publicationDate;
+}
+string MediaItem::getISBNNumber(void){
+	return this->ISBNNumber;
+}
--- OrderProject/src/MediaItem.hpp
+++ OrderProject/src/MediaItem.hpp
@@ -9,11 +9,22 @@
 #define MEDIAITEM_HPP_
 
 #include "OrderItem.hpp"
+#include "Date.hpp"
 
 class MediaItem: public OrderItem {
 public:
 	MediaItem();
 	virtual ~MediaItem();
+	void setAuthorName(string);
+	void setPublicationDate(Date);
+	void setISBNNumber(string);
+	string getAuthorName(void);
+	Date getPublicationDate(void);
+	string getISBNNumber(void);
+private:
+	string authorName;
+	Date publicationDate;
+	string ISBNNumber;
 };
 
 #endif /* MEDIAITEM_HPP_ */
