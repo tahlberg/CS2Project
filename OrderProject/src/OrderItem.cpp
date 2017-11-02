d41b0581017ae1087c84f6df9b59d3bf9ba9a5f4
Trevor Ahlberg <trevorjahlberg@gmail.com>
2017-11-02 12:52:43 -0700

    finished members/accessors for OrderItem

--- OrderProject/src/OrderItem.cpp
+++ OrderProject/src/OrderItem.cpp
@@ -16,3 +16,39 @@ OrderItem::~OrderItem() {
 	// TODO Auto-generated destructor stub
 }
 
+void OrderItem::setItemNumber(string str){
+	this->itemNumber = str;
+}
+void OrderItem::setItemDescription(string str){
+	this->itemDescription = str;
+}
+void OrderItem::setQuantity(int anInt){
+	this->quantity = anInt;
+}
+void OrderItem::setCustomerCost(double dbl){
+	this->customerCost = dbl;
+}
+void OrderItem::setVendorCost(double dbl){
+	this->vendorCost = dbl;
+}
+void OrderItem::setTaxExempt(bool aBool){
+	this->taxExempt = aBool;
+}
+string OrderItem::getItemNumber(void){
+	return this->itemNumber;
+}
+string OrderItem::getItemDescription(void){
+	return this->itemDescription;
+}
+int OrderItem::getQuantity(void){
+	return this->quantity;
+}
+double OrderItem::getCustomerCost(void){
+	return this->customerCost;
+}
+double OrderItem::getVendorCost(void){
+	return this->vendorCost;
+}
+bool OrderItem::getTaxExempt(void){
+	return this->taxExempt;
+}
--- OrderProject/src/OrderItem.hpp
+++ OrderProject/src/OrderItem.hpp
@@ -7,11 +7,32 @@
 
 #ifndef ORDERITEM_HPP_
 #define ORDERITEM_HPP_
+#include <string>
+using namespace std;
 
 class OrderItem {
 public:
 	OrderItem();
 	virtual ~OrderItem();
+	void setItemNumber(string);
+	void setItemDescription(string);
+	void setQuantity(int);
+	void setCustomerCost(double);
+	void setVendorCost(double);
+	void setTaxExempt(bool);
+	string getItemNumber(void);
+	string getItemDescription(void);
+	int getQuantity(void);
+	double getCustomerCost(void);
+	double getVendorCost(void);
+	bool getTaxExempt(void);
+private:
+	string itemNumber;
+	string itemDescription;
+	int quantity;
+	double customerCost;
+	double vendorCost;
+	bool taxExempt;
 };
 
 #endif /* ORDERITEM_HPP_ */
