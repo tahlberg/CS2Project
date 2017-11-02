c65bf8b71434de76b5061bd6877baac82a8b851d
Trevor Ahlberg <trevorjahlberg@gmail.com>
2017-11-02 13:03:29 -0700

    accessor/mutators/members for fooditem

--- OrderProject/src/FoodItem.cpp
+++ OrderProject/src/FoodItem.cpp
@@ -16,3 +16,21 @@ FoodItem::~FoodItem() {
 	// TODO Auto-generated destructor stub
 }
 
+void FoodItem::setExpirationDate(Date aDate){
+	this->expirationDate = aDate;
+}
+void FoodItem::setCalories(int anInt){
+	this->calories = anInt;
+}
+void FoodItem::setFat(int anInt){
+	this->fat = anInt;
+}
+Date FoodItem::getExpirationDate(void){
+	return this->expirationDate;
+}
+int FoodItem::getCalories(void){
+	return this->calories;
+}
+int FoodItem::getFat(void){
+	return this->fat;
+}
--- OrderProject/src/FoodItem.hpp
+++ OrderProject/src/FoodItem.hpp
@@ -14,6 +14,16 @@ class FoodItem: public OrderItem {
 public:
 	FoodItem();
 	virtual ~FoodItem();
+	void setExpirationDate(Date);
+	void setCalories(int);
+	void setFat(int);
+	Date getExpirationDate(void);
+	int getCalories(void);
+	int getFat(void);
+private:
+	Date expirationDate;
+	int calories;
+	int fat;
 };
 
 #endif /* FOODITEM_HPP_ */
