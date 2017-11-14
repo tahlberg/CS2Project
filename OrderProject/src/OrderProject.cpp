165df4281a49a11c2cecb72087bebb75e301b852
Trevor Ahlberg <trevorjahlberg@gmail.com>
2017-11-14 08:42:44 -0800

    Finished printed Order Report and implemented command line arguments

--- OrderProject/src/OrderProject.cpp
+++ OrderProject/src/OrderProject.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 void PrintOrder(Order *anOrder);
 
-int main() {
+int main(int argc, const char* argv[]) {
 	vector<Customer*> theCustomers;
 	fstream custStream("CustomerFile.txt");
 	if(custStream.fail()){
@@ -75,30 +75,78 @@ int main() {
 		tempOrder->setOrderDate(oDate);
 		theOrders.push_back(tempOrder);
 	}
-	PrintOrder(theOrders[0]);
+	if(argc == 1){
+		cout << "Order Report: " << endl;
+		cout << "===========================================" << endl;
+		for(unsigned int i = 0; i < theOrders.size(); i++){
+			PrintOrder(theOrders[i]);
+		}
+		cout << "Orders printed! Program ending. Have a nice day." << endl;
+	}
+	else if(argc == 2){
+		bool orderFound = false;
+		for(unsigned int i = 0; i < theOrders.size(); i++){
+			if(argv[1] == theOrders[i]->getOrderNumber()){
+				orderFound = true;
+				cout << "Order Report: " << endl;
+				cout << "===========================================" << endl;
+				PrintOrder(theOrders[i]);
+			}
+		}
+		cout << "Order printed! Program ending. Have a nice day." << endl;
+		if(orderFound == false){
+			cout << "Failed to find order! Program ending. Have a nice day." << endl;
+			return 0;
+		}
+	}
+	else if(argc > 2){
+		cout << "Too many parameters! Program ending. Have a nice day." << endl;
+	}
 	return 0;
 }
 
 void PrintOrder(Order *anOrder){
 	bool foodHeader = false;
-	//bool mediaHeader = false;
-	//bool electronicHeader = false;
-	cout << "Order Report: " << endl;
-	cout << "===========================================" << endl;
-	cout << right << setw(12) << "Order ID" << right << setw(15) << "Customer ID" << right << setw(12) << "Order Date" << right << setw(18) << "Customer" << right << setw(24) << "Customer Email" << right << setw(21) << "Customer Join Date" << endl;
-	cout << right << setw(12) << "--------" << right << setw(15) << "-----------" << right << setw(12) << "----------" << right << setw(18) << "--------" << right << setw(24) << "--------------" << right << setw(21) << "------------------" << endl;
-	cout << right << setw(12) << anOrder->getOrderNumber() << right << setw(15) << anOrder->getOrderCustomer().getCustomerNumber() << right << setw(12) << anOrder->getOrderDate().getDateString() << right << setw(18) << anOrder->getOrderCustomer().getCustomerName() << right << setw(24) << anOrder->getOrderCustomer().getEmail() << right << setw(21) << anOrder->getOrderCustomer().getDateJoined().getDateString() << endl;
+	bool mediaHeader = false;
+	bool electronicHeader = false;
+	cout << right << setw(12) << "Order ID" << right << setw(15) << "Customer ID" << right << setw(15) << "Order Date" << right << setw(18) << "Customer" << endl;
+	cout << right << setw(12) << "--------" << right << setw(15) << "-----------" << right << setw(15) << "----------" << right << setw(18) << "--------" << endl;
+	cout << right << setw(12) << anOrder->getOrderNumber() << right << setw(15) << anOrder->getOrderCustomer().getCustomerNumber() << right << setw(15) << anOrder->getOrderDate().getDateString() << right << setw(18) << anOrder->getOrderCustomer().getCustomerName() << endl;
 	for(unsigned int i = 0; i < anOrder->getItemsInOrder().size(); i++){
 		if(anOrder->getItemsInOrder()[i]->whoAmI() == "FoodItem"){
 			if(foodHeader == false){
 				foodHeader = true;
 				cout << endl;
-				cout << left << setw(24) << "Food Items Ordered:" << right << setw(12) << "Item Number" << right << setw(21) << "Item Description" << right << setw(12) << "Quantity" << right << setw(18) << "Expiration Date" << right << setw(12) << "Calories" << right << setw(9) << "Fat" << right << setw(9) << "Cost" << endl;
-				cout << right << setw(36) << "------------" << right << setw(21) << "----------------" << right << setw(12) << "--------" << right << setw(18) << "---------------" << right << setw(12) << "--------" << right << setw(9) << "---" << right << setw(9) << "----" << endl;
+				cout << left << setw(24) << "Food Items Ordered:" << right << setw(12) << "Item Number" << right << setw(21) << "Item Description" << right << setw(12) << "Calories" << right << setw(9) << "Cost" << endl;
+				cout << right << setw(36) << "------------" << right << setw(21) << "----------------"  << right << setw(12) <<  "--------" << right << setw(9) << "----" << endl;
 			}
 			FoodItem *tempFood = new FoodItem;
 			tempFood = dynamic_cast<FoodItem*>(anOrder->getItemsInOrder()[i]);
-			cout << right << setw(36) << tempFood->getItemNumber() << right << setw(21) << tempFood->getItemDescription() << right << setw(12) << tempFood->getQuantity() << right << setw(18) << tempFood->getExpirationDate().getDateString() << right << setw(12) << tempFood->getCalories() << right << setw(9) << tempFood->getFat() << right << setw(9) << fixed << setprecision(2) << tempFood->getCustomerCost() << endl;
+			cout << right << setw(36) << tempFood->getItemNumber() << right << setw(21) << tempFood->getItemDescription() << right << setw(12) << tempFood->getCalories() << right << setw(9) << fixed << setprecision(2) << tempFood->getCustomerCost() << endl;
+		}
+		if(anOrder->getItemsInOrder()[i]->whoAmI() == "MediaItem"){
+			if(mediaHeader == false){
+				mediaHeader = true;
+				cout << endl;
+				cout << left << setw(24) << "Media Items Ordered:" << right << setw(12) << "Item Number" << right << setw(21) << "Item Description" << right << setw(12) << "ISBN" << right << setw(9) << "Cost" << endl;
+				cout << right << setw(36) << "------------" << right << setw(21) << "----------------"  << right << setw(12) << "----" << right << setw(9) << "----" << endl;
+			}
+			MediaItem *tempMedia = new MediaItem;
+			tempMedia = dynamic_cast<MediaItem*>(anOrder->getItemsInOrder()[i]);
+			cout << right << setw(36) << tempMedia->getItemNumber() << right << setw(21) << tempMedia->getItemDescription() << right << setw(12) << tempMedia->getISBNNumber() << right << setw(9) << fixed << setprecision(2) << tempMedia->getCustomerCost() << endl;
+		}
+		if(anOrder->getItemsInOrder()[i]->whoAmI() == "ElectronicItem"){
+			if(electronicHeader == false){
+				electronicHeader = true;
+				cout << endl;
+				cout << left << setw(24) << "Electronic Items Ordered:" << right << setw(12) << "Item Number" << right << setw(21) << "Item Description" << right << setw(12) << "Warranty" << right << setw(9) << "Cost" << endl;
+				cout << right << setw(36) << "------------" << right << setw(21) << "----------------"  << right << setw(12) << "--------" << right << setw(9) << "----" << endl;
+			}
+			ElectronicItem *tempElec = new ElectronicItem;
+			tempElec = dynamic_cast<ElectronicItem*>(anOrder->getItemsInOrder()[i]);
+			cout << right << setw(36) << tempElec->getItemNumber() << right << setw(21) << tempElec->getItemDescription() << right << setw(12) << tempElec->getWarrantyMonths() << right << setw(9) << fixed << setprecision(2) << tempElec->getCustomerCost() << endl;
 		}
 	}
+	cout << "Order Total is: " << anOrder->getTotalOfOrder() << endl;
+	cout << "===========================================" << endl;
 }
